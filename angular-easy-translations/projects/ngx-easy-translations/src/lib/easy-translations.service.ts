815cd748cdd6c5e9f3816063121b56bbe0cdfc80
Fernando Maldonado <f_maldonado6@hotmail.com>
2024-02-28 23:39:50 -0600

    Added angular dependency

--- angular-easy-translations/projects/ngx-easy-translations/src/lib/easy-translations.service.ts
+++ angular-easy-translations/projects/ngx-easy-translations/src/lib/easy-translations.service.ts
@@ -1,9 +1,47 @@
-import { Injectable } from '@angular/core';
+import { EnvironmentProviders, Injectable, Provider, computed, signal } from '@angular/core';
+import { Language, TranslationConfig, translationService } from 'easy-translations-core';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class EasyTranslationsService {
+  private _language = signal<Language | null>(null)
 
-  constructor() { }
+  language = computed(() => {
+    if (this._language() == null) return
+    return this._language()
+  })
+
+  constructor(config: TranslationConfig) {
+    translationService.init(config)
+    translationService.onLanguageChanged((lang) => {
+      this._language.set(lang)
+    })
+  }
+
+  changeLanuage(language: Language) {
+    return translationService.changeLanguage(language)
+  }
+
+  changeLanguageByCode(code: string) {
+    return translationService.changeLanguageByCode(code)
+  }
+
+
+  getString(key: string, ...args: any) {
+    return computed(() => {
+      return this._language()?.getString(key, args)
+    })()
+  }
+  getStringInstant(key: string, ...args: any) {
+    return translationService.getString(key, args)
+  }
+}
+
+
+export function provideEasyTranslations(config: TranslationConfig) {
+  return {
+    provide: EasyTranslationsService,
+    useFactory: () => {
+      return new EasyTranslationsService(config)
+    }
+  }
 }
--- angular-easy-translations/projects/ngx-easy-translations/src/public-api.ts
+++ angular-easy-translations/projects/ngx-easy-translations/src/public-api.ts
@@ -3,4 +3,3 @@
  */
 
 export * from './lib/easy-translations.service';
-export * from './lib/easy-translations.component';
